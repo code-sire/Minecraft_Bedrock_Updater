159fc1327965530b33fad9ec51127d8a78061de7
Code Sire <74974737+code-sire@users.noreply.github.com>
2021-11-15 08:32:44 -0600

    Update Run Bedrock Manager.bat
    I set the batch so that is installs the needed Python modules.

--- Run Bedrock Manager.bat
+++ Run Bedrock Manager.bat
@@ -3,9 +3,9 @@
 
 taskkill /F /IM "bedrock_server.exe" /T
 
-rem "C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python310\python.exe" -m pip install requests
-rem "C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python310\python.exe" -m pip install beautifulsoup4
-rem "C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python310\python.exe" -m pip install wget
+"C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python310\python.exe" -m pip install requests
+"C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python310\python.exe" -m pip install beautifulsoup4
+"C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python310\python.exe" -m pip install wget
 
 set scriptbase=%~dp0
 
